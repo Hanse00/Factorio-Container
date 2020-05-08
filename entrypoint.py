129ee149baf6302208e8775579818906ac688c86
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2020-05-08 15:56:18 -0700

    Toggle whitelisting
    The entrypoint now contains the logic for determining if the Factorio server will be started with, or without, the whitelist feature.

--- entrypoint.py
+++ entrypoint.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
-print("Hello World!")
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="A Factorio Server")
+    whitelist = parser.add_mutually_exclusive_group(required=True)
+    whitelist.add_argument("-w", "--whitelist", dest="whitelist", action="store_true", help="Enable the server whitelist.")
+    whitelist.add_argument("-nw", "--no-whitelist", dest="whitelist", action="store_false", help="Disable the server whitelist.")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    if args.whitelist:
+        print("Starting server with whitelist...")
+    else:
+        print("Starting server without whitelist...")
+
+
+if __name__ == "__main__":
+    main()
