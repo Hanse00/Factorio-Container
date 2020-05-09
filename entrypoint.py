4b632eba8722ed096cdfead62da57e396a7f4df4
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2020-05-08 17:02:53 -0700

    Provide default configuration files
    The entrypoint script will check the required configuration files to ensure they exist, and if not create a copy based on the example files that ship with Factorio.

--- entrypoint.py
+++ entrypoint.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python3
 
 import argparse
+import shutil
 import subprocess
 
 
 SERVER_ARGS = [
     "--mod-directory", "/opt/factorio/mods",
+    "--map-gen-settings", "/opt/factorio/config/map-gen-settings.json",
+    "--map-settings", "/opt/factorio/config/map-settings.json",
+    "--server-settings", "/opt/factorio/config/server-settings.json",
+    "--server-banlist", "/opt/factorio/config/server-banlist.json",
+    "--server-adminlist", "/opt/factorio/config/server-adminlist.json",
 ]
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="A Factorio Server")
-    whitelist = parser.add_mutually_exclusive_group(required=True)
-    whitelist.add_argument("-w", "--whitelist", dest="whitelist", action="store_true", help="Enable the server whitelist.")
-    whitelist.add_argument("-nw", "--no-whitelist", dest="whitelist", action="store_false", help="Disable the server whitelist.")
-    parser.add_argument("-s", "--save-name", required=True, help="The name of the savefile to load (Eg. default.zip).")
-    args = parser.parse_args()
-    return args
+CONFIG_FILES = [
+    {
+        "path": "/opt/factorio/config/map-gen-settings.json",
+        "template": "/opt/factorio/data/map-gen-settings.example.json"
+    },
+    {
+        "path": "/opt/factorio/config/map-settings.json",
+        "template": "/opt/factorio/data/map-settings.example.json"
+    },
+    {
+        "path": "/opt/factorio/config/server-settings.json",
+        "template": "/opt/factorio/data/server-settings.example.json"
+    },
+    {
+        "path": "/opt/factorio/config/server-whitelist.json",
+        "template": "/opt/factorio/data/server-whitelist.example.json"
+    },
+]
 
 
 def main():
     args = parse_args()
+    populate_config_files()
     run_args = ["/opt/factorio/bin/x64/factorio", "--start-server"]
 
-    save_path = f"/opt/factorio/saves/{args.save_name}"
+    save_path = f"/opt/factorio/saves/{args.save}"
     run_args.append(save_path)
 
     if args.whitelist:
@@ -34,6 +51,33 @@ def main():
     
     run_args.extend(SERVER_ARGS)
     subprocess.call(run_args)
+
+
+def populate_config_files():
+    for config in CONFIG_FILES:
+        if file_is_accessible(config["path"]):
+            continue
+        else:
+            shutil.copy(config["template"], config["path"])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="A Factorio Server")
+    whitelist = parser.add_mutually_exclusive_group(required=True)
+    whitelist.add_argument("--whitelist", dest="whitelist", action="store_true", help="Enable the server whitelist.")
+    whitelist.add_argument("--no-whitelist", dest="whitelist", action="store_false", help="Disable the server whitelist.")
+    parser.add_argument("-s", "--save-file", dest="save", required=True, help="The name of the savefile to load (Eg. default.zip).")
+    args = parser.parse_args()
+    return args
+
+
+def file_is_accessible(path):
+    try:
+        f = open(path)
+        f.close()
+    except IOError:
+        return False
+    return True
 
 
 if __name__ == "__main__":
